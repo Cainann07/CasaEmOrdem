3a4ea6f91004707c411a6fbaff0f1531b1a867b0
Cainann07 <mateuscainan14@gmail.com>
2024-10-03 22:09:53 -0300

    Adição de elementos na pagina de cadastro

---
+++ app/src/main/java/com/example/casaemordem/CadastroActivity.java
@@ -0,0 +1,34 @@
+package com.example.casaemordem;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cadastro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageView fotoPerfil = findViewById(R.id.imageView4);
+        Glide.with(this)
+                .load(R.drawable.foto_de_perfil_generica)
+                .circleCrop()
+                .into(fotoPerfil);
+
+    }
+}
