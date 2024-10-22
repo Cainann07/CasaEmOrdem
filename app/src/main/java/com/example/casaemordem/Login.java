2143eaf54b0be30fc72493eb6f3a21ce156451cc
Silvspedro <pedro.asilva03@gmail.com>
2024-10-22 17:29:42 -0300

    Correção de bugs

--- app/src/main/java/com/example/casaemordem/Login.java
+++ app/src/main/java/com/example/casaemordem/Login.java
@@ -32,9 +32,9 @@ public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+       // super.onCreate(savedInstanceState);
+       // EdgeToEdge.enable(this);
+       // setContentView(R.layout.activity_login);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
