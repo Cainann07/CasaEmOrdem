df0224af74832ad7d61b0fbfb7c90b11c543c688
Silvspedro <pedro.asilva03@gmail.com>
2024-11-08 21:23:40 -0300

    back-end ProfissionalDetalhado e ProfissionaisListViewActivity corrigido indexamento com prox pagina

--- app/src/main/java/com/example/casaemordem/ProfissionalDetalhado.java
+++ app/src/main/java/com/example/casaemordem/ProfissionalDetalhado.java
@@ -2,17 +2,18 @@ package com.example.casaemordem;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.casaemordem.databinding.ActivityDetailedBinding;
+import com.example.casaemordem.databinding.ActivityProfissionalDetalhadoBinding;
 
-public class DetailedActivity extends AppCompatActivity {
-    ActivityDetailedBinding binding;
+public class ProfissionalDetalhado extends AppCompatActivity {
+    ActivityProfissionalDetalhadoBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityDetailedBinding.inflate(getLayoutInflater());
+        binding = ActivityProfissionalDetalhadoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         Intent intent = this.getIntent();
         if (intent != null){
@@ -27,5 +28,13 @@ public class DetailedActivity extends AppCompatActivity {
             binding.detailIngredients.setText(ingredients);
             binding.detailImage.setImageResource(image);
         }
+    }
+
+    public void Voltar(View view){
+        startActivity(new Intent(this, ProfissionaisListViewActivity.class));
+    }
+
+    public void Proseguir(View view){
+        startActivity(new Intent(this, InfoEnderecoClienteActivity.class));
     }
 }
