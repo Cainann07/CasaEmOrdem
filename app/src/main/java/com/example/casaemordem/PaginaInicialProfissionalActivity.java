1f168d4e33c7db27b2d89741481406a4b4c73fcb
Silvspedro <pedro.asilva03@gmail.com>
2024-11-25 16:16:32 -0300

    Mexendo na pagina Do profissional

--- app/src/main/java/com/example/casaemordem/PaginaInicialProfissionalActivity.java
+++ app/src/main/java/com/example/casaemordem/PaginaInicialProfissionalActivity.java
@@ -1,12 +1,17 @@
 package com.example.casaemordem;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 public class PaginaInicialProfissionalActivity extends AppCompatActivity {
 
@@ -15,10 +20,36 @@ public class PaginaInicialProfissionalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pagina_inicial_profissional);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationViewProfissional = findViewById(R.id.bottomNavigationViewProfissional);
+
+        SliderProfissional();
+
+        bottomNavigationViewProfissional.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.btnMenuHomeProfissional) {
+                startActivity(new Intent(this, PaginaInicialProfissionalActivity.class));
+            } else if (item.getItemId() == R.id.btnMenuChatProfissional) {
+                startActivity(new Intent(this, ChatProfissionalActivity.class));
+            } else if (item.getItemId() == R.id.fab_bottom) {
+                startActivity(new Intent(this, ContatarServicoActivity.class));
+            } else if (item.getItemId() == R.id.btnMenuCalendarioProfissional) {
+                startActivity(new Intent(this, CalendarioActivity.class));
+            } else if (item.getItemId() == R.id.btnMenuUsuarioProfissional) {
+                startActivity(new Intent(this, InfoEnderecoClienteActivity.class));
+            }
+            return true;
         });
+    }
+
+    public void SliderProfissional() {
+        ImageSlider imageSliderProfissional = findViewById(R.id.imageSliderProfissional);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.imgpromocaolimpeza, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.imgpromocaoeletrica, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.imgpromocaohidraulica, ScaleTypes.FIT));
+
+        imageSliderProfissional.setImageList(slideModels, ScaleTypes.FIT);
     }
 }
